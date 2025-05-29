0c6b1c509ad6be1741cdc9acb92076d7d856676e
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-30 00:57:11 +0200

    feat: add SkillComponent with HTML structure and placeholder images for animations

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { SectionNavComponent } from './section-nav/section-nav.component';
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [HeroComponent, SectionNavComponent, AboutMeComponent],
+  imports: [HeroComponent, SectionNavComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
