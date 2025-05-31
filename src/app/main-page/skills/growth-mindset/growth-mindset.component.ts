7dd2b9eea218ad3b990f157c6479b93553cae33e
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-05-31 14:01:45 +0200

    feat: add GrowthMindsetComponent and integrate into SkillsComponent

---
+++ src/app/main-page/skills/growth-mindset/growth-mindset.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-growth-mindset',
+  imports: [],
+  templateUrl: './growth-mindset.component.html',
+  styleUrl: './growth-mindset.component.scss'
+})
+export class GrowthMindsetComponent {
+
+}
--- src/app/main-page/skills/skills.component.ts
+++ src/app/main-page/skills/skills.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { GrowthMindsetComponent } from './growth-mindset/growth-mindset.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [GrowthMindsetComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
