317f8ada9de303372a073c94bb4f1884f341fc56
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-01 18:48:39 +0200

    feat: add ProjectComponent with styles and use in PortfolioComponent

--- src/app/main-page/portfolio/portfolio.component.ts
+++ src/app/main-page/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ShowcaseDataService } from '../showcase-data.service';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [ProjectComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
---
+++ src/app/main-page/portfolio/project/project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimaryButtonComponent } from "../../../shared/primary-button/primary-button.component";
+
+@Component({
+  selector: 'app-project',
+  imports: [PrimaryButtonComponent],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.scss'
+})
+export class ProjectComponent {
+
+}
