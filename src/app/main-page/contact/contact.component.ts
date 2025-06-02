912f2ef21ddb10b180a2c2f27586f9e5411d8422
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-02 18:08:35 +0200

    feat: add custom checkbox and warning message to ContactComponent form

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from '../../shared/primary-button/primary-button.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [PrimaryButtonComponent],
+  // ngModel in form tag requires standalone or name attribute
+  standalone: true,
+  imports: [PrimaryButtonComponent, CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  isChecked:boolean = false;
+
+  toggleCheckedState(){
+    this.isChecked = !this.isChecked;
+  }
 
 }
