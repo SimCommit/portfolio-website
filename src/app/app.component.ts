5f152773da08c80e9d842cd3f22db70574387b59
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-03 16:36:57 +0200

    feat: add scroll-down animation to HeroComponent and fallback background color logic to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { MenuOverlayComponent } from "./shared/header/menu-overlay/menu-overlay.component";
-import { HeroComponent } from './main-page/hero/hero.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageStateService } from './main-page/main-page-state.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuOverlayComponent, MainPageComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuOverlayComponent, MainPageComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio-website';
+
+    mainPageState = inject(MainPageStateService);
+
 }
--- src/app/main-page/main-page-state.service.ts
+++ src/app/main-page/main-page-state.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MainPageStateService {
+  currentSection: number = 1;
 
-  currentSection:number = 3;
+  currentBackground: string[] = [
+    '#679AAC',
+    '#F8F7E5',
+    '#1D1D1D',
+    '#F8F7E5',
+    '#679AAC',
+    '#1D1D1D',
+    '',
+  ];
 
-  mobileView:boolean = false;
+  mobileView: boolean = false;
 
-  constructor() { }
+  constructor() {}
 }
