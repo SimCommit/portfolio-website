df636bac223174e5a7605265ee9c52c60d06afe5
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-05 15:34:28 +0200

    feat: add LegalNoticeComponent with basic structure and styling

---
+++ src/app/legal-notice/legal-notice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-legal-notice',
+  imports: [FooterComponent],
+  templateUrl: './legal-notice.component.html',
+  styleUrl: './legal-notice.component.scss'
+})
+export class LegalNoticeComponent {
+
+}
