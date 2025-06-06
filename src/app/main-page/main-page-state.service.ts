8b08b998c39076f8393db69ac57825d09d577610
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-06 19:16:16 +0200

    feat: change burger menu icon from img to inline SVG for smooth color transitions

--- src/app/main-page/main-page-state.service.ts
+++ src/app/main-page/main-page-state.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MainPageStateService {
-  currentSection: number = 3;
+  currentSection: number = 1;
 
   currentBackground: string[] = [
     '',
