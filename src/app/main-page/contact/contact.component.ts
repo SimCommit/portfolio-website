f5a79ddafbae75b15eaebeceb97659c8ba0b878c
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-07 15:36:08 +0200

    feat: insert email form logic from tutorial with contactData mapping

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -11,29 +11,57 @@ import { RouterLink } from '@angular/router';
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  submittedContact: ContactData = {
+  
+  nameInput: InputFieldStates = {
+    isChecked: false,
+  };
+  
+  checkboxInput: InputFieldStates = {
+    isChecked: false,
+  };
+  
+  toggleCheckedState(inputToToggle: InputFieldStates): void {
+    inputToToggle.isChecked = !inputToToggle.isChecked;
+  }
+  
+  contactData: ContactDataModel = {
     name: '',
     email: '',
     message: '',
   };
 
+ mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.submittedContact);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      // this.http.post(this.post.endPoint, this.post.body(this.contactData))
+      //   .subscribe({
+      //     next: (response) => {
+
+      //       ngForm.resetForm();
+      //     },
+      //     error: (error) => {
+      //       console.error(error);
+      //     },
+      //     complete: () => console.info('send post complete'),
+      //   });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 
-  nameInput: InputFieldStates = {
-    isChecked: false,
-  };
-
-  checkboxInput: InputFieldStates = {
-    isChecked: false,
-  };
-
-  toggleCheckedState(inputToToggle: InputFieldStates): void {
-    inputToToggle.isChecked = !inputToToggle.isChecked;
-  }
 
 }
 
@@ -41,7 +69,7 @@ interface InputFieldStates {
   isChecked: boolean;
 }
 
-interface ContactData {
+interface ContactDataModel {
   name: string;
   email: string;
   message: string;
