a828ef63f2ab78d5f3d621053288d5d209e5ea35
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-07 23:19:52 +0200

    feat: implement initial translation setup with working language switcher

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,24 +3,45 @@ import { SocialLinksComponent } from '../social-links/social-links.component';
 import { CommonModule } from '@angular/common';
 import { MainPageStateService } from '../../main-page/main-page-state.service';
 import { MenuOverlayComponent } from '../../overlays/menu-overlay/menu-overlay.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [SocialLinksComponent, MenuOverlayComponent, CommonModule],
+  imports: [
+    SocialLinksComponent,
+    MenuOverlayComponent,
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
   mainPageState = inject(MainPageStateService);
 
-  isEnglish: Boolean = false; 
+  isEnglish: Boolean = false;
 
-  toggleLang():void {
-    this.isEnglish = !this.isEnglish;
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use(this.translate.getBrowserLang() || 'en');
   }
+
+  // toggleLang(): void {
+  //   this.isEnglish = !this.isEnglish;
+  // }
 
   openMenu(): void {
     this.mainPageState.hideMenu = false;
+  }
+
+  toggleLanguage(): void {
+    if (this.isEnglish) {
+      this.isEnglish = false;
+      this.translate.use('de');
+    } else {
+      this.isEnglish = true;
+      this.translate.use('en');
+    }
   }
 }
