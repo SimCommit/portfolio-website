08bbf3d8970e877158967cd792ef7364ccaa49b3
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-08 15:16:36 +0200

    feat: add PrivacyPolicyDeComponent, add German content to it, and register route in AppRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { PrivacyPolicyEnComponent } from './privacy-policy/privacy-policy-en/privacy-policy-en.component';
+import { PrivacyPolicyDeComponent } from './privacy-policy/privacy-policy-de/privacy-policy-de.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
@@ -11,7 +12,7 @@ export const routes: Routes = [
     path: 'privacy-policy',
     component: PrivacyPolicyComponent,
     children: [
-    // { path: 'de', component: PrivacyPolicyDeComponent },
+    { path: 'de', component: PrivacyPolicyDeComponent },
     { path: 'en', component: PrivacyPolicyEnComponent },
     { path: '', redirectTo: 'de', pathMatch: 'full' }
     ],
