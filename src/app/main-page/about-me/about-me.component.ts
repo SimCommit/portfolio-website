9dc3ddc3bd93c0667500abeee8bba3e7323a9292
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-09 16:06:14 +0200

    feat: add slide-in animation to overlay in AboutMeComponent

--- src/app/main-page/about-me/about-me.component.ts
+++ src/app/main-page/about-me/about-me.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { AboutMeOverlayComponent } from './about-me-overlay/about-me-overlay.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-me',
-  imports: [AboutMeOverlayComponent,TranslateModule],
+  imports: [AboutMeOverlayComponent, TranslateModule, CommonModule],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
+  emojiIsHovered: boolean = false;
 
+  changeHoverState(state: boolean): void {
+    this.emojiIsHovered = state;
+  }
 }
