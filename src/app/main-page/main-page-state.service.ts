4e9ba862d41ad79a4d4e72f2453557db17ab3a98
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-11 20:19:49 +0200

    feat: add responsive styling to SkillsComponent

--- src/app/main-page/main-page-state.service.ts
+++ src/app/main-page/main-page-state.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MainPageStateService {
-  currentSection: number = 1;
+  currentSection: number = 3;
 
   anchors: string[] = [
     '#',
