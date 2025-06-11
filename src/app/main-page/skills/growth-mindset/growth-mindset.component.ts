4f7db04ed40bfaa606d3248f6d50a5abd4f95228
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-11 20:27:39 +0200

    feat: show close button on touch devices in GrowthMindsetComponent

--- src/app/main-page/skills/growth-mindset/growth-mindset.component.ts
+++ src/app/main-page/skills/growth-mindset/growth-mindset.component.ts
@@ -1,17 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ShowcaseDataService } from '../../showcase-data.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { MainPageStateService } from '../../main-page-state.service';
 
 @Component({
   selector: 'app-growth-mindset',
   imports: [TranslateModule],
   templateUrl: './growth-mindset.component.html',
-  styleUrl: './growth-mindset.component.scss'
+  styleUrl: './growth-mindset.component.scss',
 })
 export class GrowthMindsetComponent {
+  mainPageState = inject(MainPageStateService);
 
-  showcaseData = inject(ShowcaseDataService)
-
+  showcaseData = inject(ShowcaseDataService);
 }
-
-
