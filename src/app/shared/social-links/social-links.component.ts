faae9144e2eeb8f9cfde32d09632f57b06b610bb
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-06-16 20:54:58 +0200

    feat: replace background images with inline SVG in social links with context-based stroke color

--- src/app/shared/social-links/social-links.component.ts
+++ src/app/shared/social-links/social-links.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-social-links',
   imports: [],
   templateUrl: './social-links.component.html',
-  styleUrl: './social-links.component.scss'
+  styleUrl: './social-links.component.scss',
 })
 export class SocialLinksComponent {
-
+  @Input() isInBurgerMenu: boolean = false;
 }
