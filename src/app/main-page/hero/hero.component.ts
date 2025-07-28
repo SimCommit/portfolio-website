5bb0c51d657e7b4a957d5bf59aab8433c7e099e2
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-28 14:29:07 +0200

    feat: add second ResizeObserver to dynamically position cogwheel and text in HeroComponent

--- src/app/main-page/hero/hero.component.ts
+++ src/app/main-page/hero/hero.component.ts
@@ -9,63 +9,103 @@ import { MainPageScrollService } from "../main-page-scroll.service";
   styleUrl: "./hero.component.scss",
 })
 export class HeroComponent {
-
   @ViewChild("heroSection") heroSectionRef!: ElementRef<HTMLElement>;
   @ViewChild("heroContent") heroContentRef!: ElementRef<HTMLElement>;
+  @ViewChild("heroText") heroTextRef!: ElementRef<HTMLElement>;
   @ViewChild("heroHeadline") heroHeadlineRef!: ElementRef<HTMLElement>;
   @ViewChild("heroGreeting") heroGreetingRef!: ElementRef<HTMLElement>;
   @ViewChild("heroPosition") heroPositionRef!: ElementRef<HTMLElement>;
+  @ViewChild("heroCogwheel") heroCogwheelRef!: ElementRef<HTMLElement>;
 
-  private resizeObserver!: ResizeObserver;
+  private headlineObserver!: ResizeObserver;
+  private offsetObserver!: ResizeObserver;
 
   constructor(public mainPageScrollService: MainPageScrollService) {}
 
   ngAfterViewInit(): void {
-    this.initResizeObserver();
+    this.initHeadlineScaleObserver();
+    this.initSectionOffsetObserver();
     window.addEventListener("resize", this.updateSectionHeight);
     this.updateSectionHeight();
   }
 
-  initResizeObserver(): void {
+  initHeadlineScaleObserver(): void {
     const contentEl = this.heroContentRef.nativeElement;
     const headlineEl = this.heroHeadlineRef.nativeElement;
     const greetingTaglineEl = this.heroGreetingRef.nativeElement;
     const positionTaglineEl = this.heroPositionRef.nativeElement;
+    const cogwheelEl = this.heroCogwheelRef.nativeElement;
+
     const MAX_HEADLINE_SIZE: number = 140;
     const MAX_TAGLINE_SIZE: number = 61;
+    const MAX_COGWHEEL_SIZE: number = 148;
+    // const MIN_COGWHEEL_SIZE: number = 80;
+    // const MAX_COGWHEEL_BOTTOM: number = 112;
 
-    this.resizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    this.headlineObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
       for (const entry of entries as ResizeObserverEntry[]) {
         const containerWidth = entry.contentRect.width;
+        // const containerHeight = entry.contentRect.height;
+
         headlineEl.style.fontSize = `${Math.min(containerWidth * 0.16, MAX_HEADLINE_SIZE)}px`;
         greetingTaglineEl.style.fontSize = `${Math.min(containerWidth * 0.07, MAX_TAGLINE_SIZE)}px`;
         positionTaglineEl.style.fontSize = `${Math.min(containerWidth * 0.07, MAX_TAGLINE_SIZE)}px`;
+        cogwheelEl.style.width = `${Math.min(containerWidth * 0.3, MAX_COGWHEEL_SIZE)}px`;
+        cogwheelEl.style.height = `${Math.min(containerWidth * 0.3, MAX_COGWHEEL_SIZE)}px`;
       }
     });
 
-    this.resizeObserver.observe(contentEl);
+    this.headlineObserver.observe(contentEl);
+  }
+
+  initSectionOffsetObserver() {
+    const sectionEl = this.heroSectionRef.nativeElement;
+    const cogwheelEl = this.heroCogwheelRef.nativeElement;
+    const textEl = this.heroTextRef.nativeElement;
+
+    this.offsetObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+      for (const entry of entries as ResizeObserverEntry[]) {
+        const windowWidth = window.innerWidth;
+        const sectionHeight = entry.contentRect.height;
+        const multiplier = sectionHeight / 600;
+        console.log(sectionHeight);
+        console.log(multiplier);
+
+        if (windowWidth > 800) {
+          textEl.style.marginTop = `${sectionHeight * 0.07 * multiplier}px`;
+          cogwheelEl.style.bottom = `${sectionHeight * 0.05 * multiplier}px`;
+        } else {
+          textEl.style.marginTop = `${40}px`;
+          cogwheelEl.style.bottom = `${40}px`;
+        }
+      }
+    });
+
+    this.offsetObserver.observe(sectionEl);
   }
 
   updateSectionHeight = (): void => {
-    const sectionElement = this.heroSectionRef.nativeElement;
+    const sectionEl = this.heroSectionRef.nativeElement;
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
     const sectionWidth = Math.min(windowWidth, 1600);
+    const sectionHeight = sectionWidth * 0.6;
     const MAX_SECTION_ASPECT_RATIO: number = 1.5;
     const BREAKPOINT_MOBILE: number = 800;
     const MIN_SECTION_HEIGHT: number = 440;
 
     if (sectionWidth / windowHeight <= MAX_SECTION_ASPECT_RATIO && windowWidth >= BREAKPOINT_MOBILE) {
-      sectionElement.style.height = `${Math.max(sectionWidth * 0.6, MIN_SECTION_HEIGHT)}px`;
+      sectionEl.style.height = `${Math.max(sectionHeight, MIN_SECTION_HEIGHT)}px`;
     } else if (windowWidth < BREAKPOINT_MOBILE) {
-      sectionElement.style.height = "unset";
+      sectionEl.style.height = "unset";
     } else {
-      sectionElement.style.height = `clamp(640px, 100dvh, 1200px)`;
+      sectionEl.style.height = `clamp(640px, 100dvh, 1200px)`;
     }
   };
 
   ngOnDestroy(): void {
-    this.resizeObserver?.disconnect();
+    this.headlineObserver?.disconnect();
+    this.offsetObserver?.disconnect();
     window.removeEventListener("resize", this.updateSectionHeight);
   }
 }
