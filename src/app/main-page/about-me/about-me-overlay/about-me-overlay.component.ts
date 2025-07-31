1a0cfb5526683b1240602748d16c212deb01f567
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-07-31 12:45:44 +0200

    feat: extend AboutMeComponent with mobile responsiveness

--- src/app/main-page/about-me/about-me-overlay/about-me-overlay.component.ts
+++ src/app/main-page/about-me/about-me-overlay/about-me-overlay.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { PageStateService } from '../../../page-state.service';
+import { BreakpointObserverService } from '../../../breakpoint-observer.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-about-me-overlay',
-  imports: [TranslateModule],
+  imports: [TranslateModule, AsyncPipe],
   templateUrl: './about-me-overlay.component.html',
   styleUrl: './about-me-overlay.component.scss'
 })
 export class AboutMeOverlayComponent {
 
-      constructor(public pageStateService: PageStateService) {}
+      constructor(public breakpointObserverService: BreakpointObserverService) {}
     
 
 }
