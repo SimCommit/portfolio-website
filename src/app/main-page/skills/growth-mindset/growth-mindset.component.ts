aefe962af15b1d05f8b40bd836799310e81d1b2b
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-08-01 16:56:33 +0200

    feat: improve mobile positioning of GrowthMindsetComponent

--- src/app/main-page/skills/growth-mindset/growth-mindset.component.ts
+++ src/app/main-page/skills/growth-mindset/growth-mindset.component.ts
@@ -1,17 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { ShowcaseDataService } from '../../showcase-data.service';
-import { TranslateModule } from '@ngx-translate/core';
-import { PageStateService } from '../../../page-state.service';
+import { Component } from "@angular/core";
+import { ShowcaseDataService } from "../../showcase-data.service";
+import { TranslateModule } from "@ngx-translate/core";
+import { BreakpointObserverService } from "../../../breakpoint-observer.service";
+import { AsyncPipe } from "@angular/common";
 
 @Component({
-  selector: 'app-growth-mindset',
-  imports: [TranslateModule],
-  templateUrl: './growth-mindset.component.html',
-  styleUrl: './growth-mindset.component.scss',
+  selector: "app-growth-mindset",
+  imports: [TranslateModule, AsyncPipe],
+  templateUrl: "./growth-mindset.component.html",
+  styleUrl: "./growth-mindset.component.scss",
 })
 export class GrowthMindsetComponent {
-
-  showcaseData = inject(ShowcaseDataService);
-
-  constructor(public pageStateService: PageStateService) {}
+  constructor(public showcaseData: ShowcaseDataService, public breakpointObserverService: BreakpointObserverService) {}
 }
