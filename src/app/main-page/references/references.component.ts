22e8076b2d4e6c89cffd3db68d921d1af41da8e2
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-08-03 16:27:36 +0200

    feat: improve ReferencesComponent responsiveness from 320px to 4K

--- src/app/main-page/references/references.component.ts
+++ src/app/main-page/references/references.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
-import { ColleagueQuoteComponent } from './colleague-quote/colleague-quote.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { ColleagueQuoteComponent } from "./colleague-quote/colleague-quote.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { SectionLayoutService } from "../services/section-layout.service";
+import { AsyncPipe, CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-references',
-  imports: [ColleagueQuoteComponent, TranslateModule],
-  templateUrl: './references.component.html',
-  styleUrl: './references.component.scss'
+  selector: "app-references",
+  imports: [ColleagueQuoteComponent, TranslateModule, AsyncPipe, CommonModule],
+  templateUrl: "./references.component.html",
+  styleUrl: "./references.component.scss",
 })
 export class ReferencesComponent {
+  constructor(public sectionLayoutService: SectionLayoutService) {}
 
-
-  
+  ngOnInit(): void {
+    this.sectionLayoutService.startViewportObserver();
+  }
 }
