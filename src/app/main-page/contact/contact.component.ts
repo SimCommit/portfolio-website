5c371117185d84c956c2170fddae67927f4dc80e
SimCommit <168715756+SimCommit@users.noreply.github.com>
2025-08-03 22:33:30 +0200

    chore: WIP adjust content width for ContactComponent layout

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,18 +1,19 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
-import { FooterComponent } from '../../shared/footer/footer.component';
-import { RouterLink } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { PageStateService } from '../../page-state.service';
-import { MainPageScrollService } from '../main-page-scroll.service';
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, NgForm } from "@angular/forms";
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { RouterLink } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { TranslateModule } from "@ngx-translate/core";
+import { PageStateService } from "../../page-state.service";
+import { MainPageScrollService } from "../main-page-scroll.service";
+import { SectionLayoutService } from "../services/section-layout.service";
 
 @Component({
-  selector: 'app-contact',
+  selector: "app-contact",
   imports: [CommonModule, FormsModule, FooterComponent, RouterLink, TranslateModule],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
+  templateUrl: "./contact.component.html",
+  styleUrl: "./contact.component.scss",
 })
 export class ContactComponent {
   http = inject(HttpClient);
@@ -25,44 +26,50 @@ export class ContactComponent {
     isChecked: false,
   };
 
-  constructor(public pageStateService: PageStateService, public mainPageScrollService: MainPageScrollService ) {}
+  constructor(
+    public pageStateService: PageStateService,
+    public mainPageScrollService: MainPageScrollService,
+    public sectionLayoutService: SectionLayoutService
+  ) {}
+
+  ngOnInit(): void {
+    this.sectionLayoutService.startViewportObserver();
+  }
 
   toggleCheckedState(inputToToggle: InputFieldStates): void {
     inputToToggle.isChecked = !inputToToggle.isChecked;
   }
 
   contactData: ContactDataModel = {
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   };
 
   mailTest = true;
 
   post = {
-    endPoint: 'https://simon-fuchs.developerakademie.net/angular-projects/simon-fuchs/sendMail.php',
+    endPoint: "https://simon-fuchs.developerakademie.net/angular-projects/simon-fuchs/sendMail.php",
     body: (payload: any) => JSON.stringify(payload),
     options: {
       headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
+        "Content-Type": "text/plain",
+        responseType: "text",
       },
     },
   };
 
   onSubmit(ngForm: NgForm) {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http
-        .post(this.post.endPoint, this.post.body(this.contactData))
-        .subscribe({
-          next: (response) => {
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error(error);
-          },
-          complete: () => console.info('send post complete'),
-        });
+      this.http.post(this.post.endPoint, this.post.body(this.contactData)).subscribe({
+        next: (response) => {
+          ngForm.resetForm();
+        },
+        error: (error) => {
+          console.error(error);
+        },
+        complete: () => console.info("send post complete"),
+      });
     } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
       console.log(this.contactData);
 
